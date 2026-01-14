fd4063dbd519ad87297422f0646cf0f809c012a7
Wiscers1 <michaelrileylolz9@gmail.com>
2026-01-14 18:44:06 -0500

    Add files via upload

---
+++ schedule-data.js
@@ -0,0 +1,44 @@
+const SCHEDULE = [
+    {
+        day: "Monday",
+        date: "1/12",
+        cook: "Michael",
+        clean: "Noah"
+    },
+    {
+        day: "Tuesday",
+        date: "1/13",
+        cook: "Mark Lee",
+        clean: "Emily Davis"
+    },
+    {
+        day: "Wednesday",
+        date: "1/14",
+        cook: "",
+        clean: ""
+    },
+    {
+        day: "Thursday",
+        date: "1/15",
+        cook: "Anna Brown",
+        clean: "David Wilson"
+    },
+    {
+        day: "Friday",
+        date: "1/16",
+        cook: "",
+        clean: ""
+    },
+    {
+        day: "Saturday",
+        date: "1/17",
+        cook: "Waypoint Team",
+        clean: "Waypoint Team"
+    },
+    {
+        day: "Sunday",
+        date: "1/18",
+        cook: "",
+        clean: ""
+    }
+];
